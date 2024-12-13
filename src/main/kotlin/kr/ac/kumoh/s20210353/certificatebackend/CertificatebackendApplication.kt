e0b25e85b794513dc379abea85bd6bc5e292a5fd
snowrabbit123@naver.com <snowrabbit123@naver.com>
2024-12-13 16:24:02 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210353/certificatebackend/CertificatebackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210353.certificatebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CertificatebackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CertificatebackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210353/certificatebackend/CertificatebackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210353.certificatebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CertificatebackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
